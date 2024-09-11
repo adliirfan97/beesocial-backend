5754c83c3148f90f7eb23a181879e3f8b9a214e3
nadiahafiqah <nadiaha.ismail@gmail.com>
2024-09-11 14:51:35 +0800

    added user model class

---
+++ user-management-service/src/main/java/com/beesocial/usermanagementservice/model/User.java
@@ -0,0 +1,32 @@
+package com.beesocial.usermanagementservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "User_generator")
+    @SequenceGenerator(name = "User_generator", sequenceName = "User_seq", allocationSize = 1)
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String profilePhoto;
+    private ROLE role;
+
+    public User(String firstName, String lastName, String email, String phoneNumber, String password, int role, String profilePhoto) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.role = role;
+        this.profilePhoto = profilePhoto;
+    }
+}
