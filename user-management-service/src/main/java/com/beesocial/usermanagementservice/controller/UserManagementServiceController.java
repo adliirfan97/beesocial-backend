f70518495875833e30f3f77c4d15984c55eaea1b
nadiahafiqah <nadiaha.ismail@gmail.com>
2024-09-11 14:52:23 +0800

    updated user controller with basic crud mappings

--- user-management-service/src/main/java/com/beesocial/usermanagementservice/controller/UserManagementServiceController.java
+++ user-management-service/src/main/java/com/beesocial/usermanagementservice/controller/UserManagementServiceController.java
@@ -1,13 +1,45 @@
 package com.beesocial.usermanagementservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.beesocial.usermanagementservice.model.User;
+import com.beesocial.usermanagementservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
+@RequestMapping("user")
 public class UserManagementServiceController {
+    private final UserService userService;
+
+    @Autowired
+    public UserManagementServiceController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/test")
     public String test() {
         return "Testing";
     }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable int id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping("/new")
+    public User saveNewUser(User user){
+        return userService.saveUser(user);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteExistingUserById(@PathVariable int id) {
+        userService.deleteUser(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public User updateExistingUserById(@PathVariable int id){
+        return userService.updateUser(id);
+    }
+
 }
