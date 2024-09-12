66dbbad11ac211f7b26d5070216df81dcffffb6d
adrian.kiffly <adrian.kiffly@fdmgroup.com>
2024-09-12 17:34:02 +0800

    Added: addApplicantsToEvent and Added: testAddApplicantToEvent

--- event-management-service/src/main/java/com/beesocial/eventmanagementservice/service/EventService.java
+++ event-management-service/src/main/java/com/beesocial/eventmanagementservice/service/EventService.java
@@ -7,28 +7,36 @@ import org.springframework.http.ResponseEntity;
 import java.time.LocalDateTime;
 
 public class EventService {
-    public ResponseEntity<Object> saveEvent(int userId, String text, String image){
-        Event event = new Event(userId, LocalDateTime.now());
+    public ResponseEntity<Object> saveEvent(Event event){
+        String text = event.getText();
+        String image = event.getImage();
         if((text == null || text.isEmpty()) && (image == null || image.isEmpty())){
             return ResponseEntity.badRequest().body("no text and no image");
         }
-        if(!text.isEmpty() && !image.isEmpty()){
-            event.setText(text);
-            event.setImage(image);
-        }
-        if(text.isEmpty() && !image.isEmpty()){
-            event.setImage(image);
-        }
-        if(!text.isEmpty() && image.isEmpty()){
+        if(!(text==null||text.isEmpty())){
             if(text.length()>=2000){
                 return ResponseEntity.badRequest().body("text too long");
             }
             event.setText(text);
         }
+
+        // ADD: save event to database
         return ResponseEntity.ok(event);
     }
     public ResponseEntity<Object> addApplicantById(int eventId, int userId){
+        // ADD: validation checks the existence of eventId and userId
+        // ADD: validation checks if the user has already applied to the event
+
         EventApplicant eventApplicant = new EventApplicant(eventId, userId);
+
         return ResponseEntity.ok(eventApplicant);
+    }
+    public ResponseEntity<Object> editEventById(int eventId, Event updatedEvent){
+        // ADD: find eventById
+
+        // ADD: set the new updates
+
+        // ADD: save updated event
+        return ResponseEntity.ok("updatedEvent");
     }
 }
--- event-management-service/src/test/java/com/beesocial/eventmanagementservice/service/EventServiceTest.java
+++ event-management-service/src/test/java/com/beesocial/eventmanagementservice/service/EventServiceTest.java
@@ -1,6 +1,7 @@
 package com.beesocial.eventmanagementservice.service;
 
 import com.beesocial.eventmanagementservice.model.Event;
+import com.beesocial.eventmanagementservice.model.EventApplicant;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
 
@@ -10,44 +11,67 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EventServiceTest {
     @Test
+    public void testEvent_checkTime(){
+        Event eventReturn = new Event(1);
+
+        assertEquals(LocalDateTime.now(), eventReturn.getTimestamp());
+    }
+    @Test
     public void testSaveEvent_imageAndText(){
         EventService eventService = new EventService();
 
-        Event eventReturn = new Event(1, LocalDateTime.now());
+        Event eventExpected = new Event(1);
+        eventExpected.setText("event post text");
+        eventExpected.setImage("image.png");
+
+        Event eventReturn = new Event(1);
         eventReturn.setText("event post text");
         eventReturn.setImage("image.png");
 
-        assertEquals(eventReturn, eventService.saveEvent(1, "event post text", "image.png").getBody());
+        assertEquals(eventExpected, eventService.saveEvent(eventReturn).getBody());
     }
     @Test
     public void testSaveEvent_imageOnly(){
         EventService eventService = new EventService();
 
-        Event eventReturn = new Event(1, LocalDateTime.now());
+        Event eventReturn = new Event(1);
         eventReturn.setImage("image.png");
 
-        assertEquals(eventReturn, eventService.saveEvent(1, "", "image.png").getBody());
+        Event eventExpected = new Event(1);
+        eventExpected.setImage("image.png");
+
+        assertEquals(eventExpected, eventService.saveEvent(eventReturn).getBody());
     }
     @Test
     public void testSaveEvent_textOnly(){
         EventService eventService = new EventService();
 
-        Event eventReturn = new Event(1, LocalDateTime.now());
+        Event eventReturn = new Event(1);
         eventReturn.setText("event post text");
 
-        assertEquals(eventReturn, eventService.saveEvent(1, "event post text", "").getBody());
+        Event eventExpected = new Event(1);
+        eventExpected.setText("event post text");
+
+
+        assertEquals(eventExpected, eventService.saveEvent(eventReturn).getBody());
     }
     @Test
     public void testSaveEvent_nullTextAndNullImage(){
         EventService eventService = new EventService();
 
-        assertEquals(ResponseEntity.badRequest().body("no text and no image").getStatusCode(), eventService.saveEvent(1, null, null).getStatusCode());
+        Event eventReturn = new Event(1);
+
+        assertEquals(ResponseEntity.badRequest().body("no text and no image").getStatusCode(), eventService.saveEvent(eventReturn).getStatusCode());
     }
     @Test
     public void testSaveEvent_emptyTextAndEmptyImage(){
         EventService eventService = new EventService();
 
-        assertEquals(ResponseEntity.badRequest().body("no text and no image").getStatusCode(), eventService.saveEvent(1, "", "").getStatusCode());
+        Event eventReturn = new Event(1);
+        eventReturn.setImage("");
+        eventReturn.setText("");
+
+        assertEquals(ResponseEntity.badRequest().body("no text and no image").getStatusCode(), eventService.saveEvent(eventReturn).getStatusCode());
     }
     @Test
     public void testSaveEvent_textMoreThan2000Characters(){
@@ -58,7 +82,17 @@ public class EventServiceTest {
                 "Lorem ipsum odor amet, consectetuer adipiscing elit. Maximus vestibulum donec faucibus aptent sed phasellus. Dictum nisl ad volutpat habitasse massa eleifend malesuada. Sit conubia massa imperdiet eget cursus felis diam vivamus. Facilisi pulvinar egestas consequat enim consectetur cras nulla. Eleifend quis nam quisque ultricies morbi sociosqu natoque vel laoreet. Commodo netus imperdiet consectetur porttitor nascetur tortor aliquam. Taciti consequat sodales ad quam gravida sem." +
                 "Lorem ipsum odor amet, consectetuer adipiscing elit. Maximus vestibulum donec faucibus aptent sed phasellus. Dictum nisl ad volutpat habitasse massa eleifend malesuada. Sit conubia massa imperdiet eget cursus felis diam vivamus. Facilisi pulvinar egestas consequat enim consectetur cras nulla. Eleifend quis nam quisque ultricies morbi sociosqu natoque vel laoreet. Commodo netus imperdiet consectetur porttitor nascetur tortor aliquam. Taciti consequat sodales ad quam gravida sem.";
 
-        assertEquals(ResponseEntity.badRequest().body("no text and no image").getStatusCode(), eventService.saveEvent(1, text, "").getStatusCode());
-    }
+        Event eventReturn = new Event(1);
+        eventReturn.setText(text);
 
+        assertEquals(ResponseEntity.badRequest().body("no text and no image").getStatusCode(), eventService.saveEvent(eventReturn).getStatusCode());
+    }
+    @Test
+    public void testAddApplicantById(){
+        EventService eventService = new EventService();
+
+        EventApplicant eventApplicant = new EventApplicant(1, 1);
+
+        assertEquals(eventApplicant, eventService.addApplicantById(1, 1).getBody());
+    }
 }
