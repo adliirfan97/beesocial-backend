2bfa950341309345880f3a4b6893009ee3c08aea
adrian.kiffly <adrian.kiffly@fdmgroup.com>
2024-09-12 17:35:02 +0800

    Added: timestamp as an attribute

--- event-management-service/src/main/java/com/beesocial/eventmanagementservice/model/EventApplicant.java
+++ event-management-service/src/main/java/com/beesocial/eventmanagementservice/model/EventApplicant.java
@@ -3,14 +3,18 @@ package com.beesocial.eventmanagementservice.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 public class EventApplicant {
     private int eventId;
     private int userId;
+    private LocalDateTime timestamp;
 
     public EventApplicant(int eventId, int userId) {
         this.eventId = eventId;
         this.userId = userId;
+        this.timestamp = LocalDateTime.now();
     }
 }
