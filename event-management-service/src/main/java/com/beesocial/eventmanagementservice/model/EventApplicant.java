e0a8ea4572a123a74e8896837d872c8e45e89449
adrian.kiffly <adrian.kiffly@fdmgroup.com>
2024-09-12 09:47:29 +0800

    Added: EventApplicant model as a joining table between events and users who applied

--- event-management-service/src/main/java/com/beesocial/eventmanagementservice/model/EventApplicant.java
+++ event-management-service/src/main/java/com/beesocial/eventmanagementservice/model/EventApplicant.java
@@ -1,2 +1,16 @@
-package com.beesocial.eventmanagementservice.model;public class EventApplicant {
+package com.beesocial.eventmanagementservice.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class EventApplicant {
+    private int eventId;
+    private int userId;
+
+    public EventApplicant(int eventId, int userId) {
+        this.eventId = eventId;
+        this.userId = userId;
+    }
 }
