83ce5ce17fd2a575b2972422593bed51efce5bc9
adrian.kiffly <adrian.kiffly@fdmgroup.com>
2024-09-17 11:55:08 +0800

    Edited: refractored code using feign client

--- event-management-service/src/main/java/com/beesocial/eventmanagementservice/feign/FirebaseStorageClient.java
+++ event-management-service/src/main/java/com/beesocial/eventmanagementservice/feign/FirebaseStorageClient.java
@@ -1,2 +1,18 @@
-package com.beesocial.eventmanagementservice.feign;public interface FirebaseStorageClient {
+package com.beesocial.eventmanagementservice.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
+
+@FeignClient(name = "firebase-storage-service")
+public interface FirebaseStorageClient {
+    @PostMapping("/api/firebase/{collectionName}")
+    public String saveData(@PathVariable String collectionName, @RequestBody ResponseEntity<Object> data);
+    @PutMapping("/api/firebase/{collectionName}/{documentId}")
+    public String editData(@PathVariable String collectionName, @PathVariable String documentId, @RequestBody ResponseEntity<Object> data);
 }
