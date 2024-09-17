e2a49af011e9cb57f7e670c7b60d8f98d13cfb7a
adli anuar <adli.anuar@fdmgroup.com>
2024-09-17 13:36:06 +0800

    Changed repository to feign

--- opportunity-management-service/src/main/java/com/beesocial/opportunitymanagementservice/feign/FirebaseClient.java
+++ opportunity-management-service/src/main/java/com/beesocial/opportunitymanagementservice/feign/FirebaseClient.java
@@ -1,10 +1,7 @@
-package com.beesocial.opportunitymanagementservice.repository;
+package com.beesocial.opportunitymanagementservice.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
