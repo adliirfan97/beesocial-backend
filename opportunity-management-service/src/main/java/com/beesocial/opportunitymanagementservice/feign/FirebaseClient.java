38f6d8260c6ec69aa46c04c9d69a725e4eda1367
Wilson Ong <wilsonong.hs@gmail.com>
2024-09-18 09:12:43 +0800

    Merge branch 'dev' of https://git.fdmgroup.com/adli.anuar/beesocial-backend into dev

--- opportunity-management-service/src/main/java/com/beesocial/opportunitymanagementservice/feign/FirebaseClient.java
+++ opportunity-management-service/src/main/java/com/beesocial/opportunitymanagementservice/feign/FirebaseClient.java
@@ -1,6 +1,7 @@
 package com.beesocial.opportunitymanagementservice.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
