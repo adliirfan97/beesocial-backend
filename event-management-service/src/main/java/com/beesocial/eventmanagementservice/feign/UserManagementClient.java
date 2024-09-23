d2aaabf1f0a0c6a24fc37f97d8811299e1b28ddf
adrian.kiffly <adrian.kiffly@fdmgroup.com>
2024-09-24 00:36:44 +0800

    Added: not in use now, may be used in the future

--- event-management-service/src/main/java/com/beesocial/eventmanagementservice/feign/UserManagementClient.java
+++ event-management-service/src/main/java/com/beesocial/eventmanagementservice/feign/UserManagementClient.java
@@ -1,2 +1,13 @@
-package com.beesocial.eventmanagementservice.feign;public interface UserManagementClient {
+package com.beesocial.eventmanagementservice.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@FeignClient(name = "user-management-service")
+public interface UserManagementClient {
+    @GetMapping("/user/{id}")
+    public Optional<?> getUserById(@PathVariable int id);
 }
