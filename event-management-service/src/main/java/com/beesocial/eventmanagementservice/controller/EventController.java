874edbecf1e0396a93707d4dbd4f3ca668bee881
adrian.kiffly <adrian.kiffly@fdmgroup.com>
2024-09-24 00:35:52 +0800

    Edited: changed controller

--- event-management-service/src/main/java/com/beesocial/eventmanagementservice/controller/EventController.java
+++ event-management-service/src/main/java/com/beesocial/eventmanagementservice/controller/EventController.java
@@ -1,2 +1,7 @@
-package com.beesocial.eventmanagementservice.controller;public class EventController {
+package com.beesocial.eventmanagementservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventController {
 }
