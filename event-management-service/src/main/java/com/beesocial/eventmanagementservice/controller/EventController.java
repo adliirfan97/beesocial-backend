62050d99415f6a1219fdf4355df93772be21483b
adrian.kiffly <adrian.kiffly@fdmgroup.com>
2024-09-26 23:00:20 +0800

    Added: eventController

--- event-management-service/src/main/java/com/beesocial/eventmanagementservice/controller/EventController.java
+++ event-management-service/src/main/java/com/beesocial/eventmanagementservice/controller/EventController.java
@@ -1,7 +1,29 @@
 package com.beesocial.eventmanagementservice.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.beesocial.eventmanagementservice.model.Event;
+import com.beesocial.eventmanagementservice.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EventController {
+    @Autowired
+    private EventService eventService;
+    @PostMapping
+    public ResponseEntity<?> saveEvent(@RequestBody Event event){
+        return eventService.saveEvent(event);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editEventById(@PathVariable int id, @RequestBody Event updatedEvent){
+        return eventService.editEventById(id, updatedEvent);
+    }
+    @PostMapping("/addApplicant")
+    public ResponseEntity<?> addApplicantById(@RequestParam int eventId, @RequestParam int userId){
+        return eventService.addApplicantById(eventId, userId);
+    }
+    @GetMapping
+    public ResponseEntity<?> getAllEvents(){
+        return eventService.getAllEvents();
+    }
 }
