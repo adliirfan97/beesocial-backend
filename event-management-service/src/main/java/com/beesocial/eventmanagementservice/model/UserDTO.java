2155a8782d0b67c57c4a6ec2c6b6cce3f3b95ae6
adrian.kiffly <adrian.kiffly@fdmgroup.com>
2024-09-30 09:30:51 +0800

    Removed: Entity and id gen and seq from UserDTO

--- event-management-service/src/main/java/com/beesocial/eventmanagementservice/model/UserDTO.java
+++ event-management-service/src/main/java/com/beesocial/eventmanagementservice/model/UserDTO.java
@@ -1,16 +1,12 @@
 package com.beesocial.eventmanagementservice.model;
 
-import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
 @NoArgsConstructor
 public class UserDTO {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "User_generator")
-    @SequenceGenerator(name = "User_generator", sequenceName = "User_seq", allocationSize = 1)
+
     private int userId;
     private String firstName;
     private String lastName;
