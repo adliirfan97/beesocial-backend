516cf9e5f77bea03d5c7b1359aa9a67b0f1446dd
adrian.kiffly <adrian.kiffly@fdmgroup.com>
2024-09-30 23:39:38 +0800

    Edited: post event http

--- event-management-service/src/main/java/com/beesocial/eventmanagementservice/controller/EventController.java
+++ event-management-service/src/main/java/com/beesocial/eventmanagementservice/controller/EventController.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 public class EventController {
     @Autowired
     private EventService eventService;
-    @PostMapping
+    @PostMapping("/events")
     public ResponseEntity<?> saveEvent(@RequestBody Event event){
         return eventService.saveEvent(event);
     }
